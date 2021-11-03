e1467ba4cdeb2086efe1e00790b4d65ca58d0c37
Anzor <anzors.andrejevs@gmail.com>
2021-11-03 11:01:56 +0000

    Add project files.

---
+++ WindowsFormsApp1/MyFancyClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectForUtests
+{
+    [TestClass]
+    class MyFancyClass
+    {
+        MyFancyClass my;
+
+        [ClassInitialize]
+
+        public static void TestFixtureSetup(TestContext context)
+        {
+            Debug.WriteLine("Class set up: " + context);
+
+        }
+
+
+        [TestInitialize]
+
+        public void Setup()
+        {
+            Debug.WriteLine("Hello this is setup");
+            my = new MyFancyClass(6, 7);
+        }
+
+
+        [TestMethod]
+
+        public void TestAdd()
+        {
+            Debug.WriteLine("TestAdd");
+            int sum = my.add();
+            Assert.AreEqual(13, sum);
+        }
+
+        [TestMethod]
+        public void TestSub()
+        {
+            Debug.WriteLine("TestSub");
+            int sum = my.sub();
+            Assert.AreEqual(-1, sum);
+        }
+
+        [TestCleanup]
+
+        public void Cleanup()
+        {
+            Debug.WriteLine("Cleaned");
+        
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/TestFancyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProjectForUtests
+{
+    class TestFancyClass
+    {
+        int num1;
+        int num2;
+
+        public MyFancyClass(int n1, int n2)
+        {
+            num1 = n1;
+            num2 = n2;
+
+        }
+        public int add()
+        {
+            return num1 + num2;
+        }
+        public int sub()
+        {
+            return num1 - num2;
+        }
+
+    }
+}
